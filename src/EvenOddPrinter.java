b03723fb203aaa2010d0205c37d662c184df80a3
DiegoVargas0813 <A01635782@tec.mx>
2025-04-04 20:10:51 -0600

    Second Session

--- src/EvenOddPrinter.java
+++ src/EvenOddPrinter.java
@@ -1,3 +1,24 @@
 public class EvenOddPrinter {
+    public static void main(String[] args) {
+        // Thread for even numbers
+        Thread evenThread = new Thread(() -> {
+            for (int i = 2; i <= 20; i += 2) {
+                System.out.println("Even: " + i);
+                try { Thread.sleep(100); } catch (InterruptedException e) {}
+            }
+        });
+    
+        // Thread for odd numbers
+        Thread oddThread = new Thread(() -> {
+            for (int i = 1; i <= 20; i += 2) {
+                System.out.println("Odd: " + i);
+                try { Thread.sleep(100); } catch (InterruptedException e) {}
+            }
+        });
 
+        evenThread.start();
+        oddThread.start();
+    }
 }
+
+
